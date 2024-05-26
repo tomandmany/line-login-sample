befd9a45ca45da218f7b4488ec36a6b8b29030a6
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-05-26 16:09:43 +0900

    一旦

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/votes',
+                destination: '/login',
                 permanent: true,
             },
         ]
