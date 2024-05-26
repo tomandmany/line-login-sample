bfdde04e96a736f2cfd8ca32722cd0360a0d622b
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-05-26 23:33:43 +0900

    console.logを削除

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,6 @@ const LoginPage = async () => {
         redirect("/votes");
     }
 
-    console.log('ログインページにアクセスしました！');
     return (
         <main className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-4xl mb-4">ログイン</h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 // app/page.tsx
 
 const HomePage = () => {
-  console.log('ホームページにアクセスしました！');
   return (
-    // <Providers>
-    //   <RequireAuth>
-    //     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-    //       <h1 className="text-4xl mb-4">ホームページ</h1>
-    //     </main>
-    //   </RequireAuth>
-    // </Providers>
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl mb-4">ホームページ</h1>
     </main>
